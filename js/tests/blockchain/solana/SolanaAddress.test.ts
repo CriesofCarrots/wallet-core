69c4065fa7e563c6353b392b654a31c368bfd677
Tyera Eulberg <tyera@solana.com>
2019-08-02 16:36:42 -0600

    Add js integration tests

--- js/tests/blockchain/solana/SolanaAddress.test.ts
+++ js/tests/blockchain/solana/SolanaAddress.test.ts
@@ -1,27 +1,32 @@
-import { expect } from 'chai';
 import 'mocha';
 
-import { bufToHex, fromHexString } from '../../Utils';
-import { Base58, PrivateKey, SolanaAddress, PublicKey, PublicKeyType } from '../../../lib';
+import {expect} from 'chai';
+
+import {Base58, PrivateKey, PublicKey, PublicKeyType, SolanaAddress} from '../../../lib';
+import {bufToHex, fromHexString} from '../../Utils';
 
 describe('SolanaAddress', () => {
-
     it('test address from PrivateKey', () => {
-        const key = PrivateKey.createWithData(Base58.decodeNoCheck('A7psj2GW7ZMdY4E5hJq14KMeYg7HFjULSsWSrTXZLvYr'));
+        const key = PrivateKey.createWithData(
+            Base58.decodeNoCheck('A7psj2GW7ZMdY4E5hJq14KMeYg7HFjULSsWSrTXZLvYr'));
         const pubkey = key.getPublicKeyEd25519();
         const address = SolanaAddress.createWithPublicKey(pubkey);
 
-        expect(Base58.encodeNoCheck(pubkey.data())).to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
+        expect(Base58.encodeNoCheck(pubkey.data()))
+            .to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
         expect(address.description()).to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
     });
 
     it('test address from PublicKey', () => {
-        const pubkey = PublicKey.createWithData(Base58.decodeNoCheck('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q'), PublicKeyType.ED25519);
+        const pubkey = PublicKey.createWithData(
+            Base58.decodeNoCheck('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q'),
+            PublicKeyType.ED25519);
         const addressPubkey = SolanaAddress.createWithPublicKey(pubkey);
-        const addressString = SolanaAddress.createWithString('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
+        const addressString =
+            SolanaAddress.createWithString('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
 
         // expect(addressPubkey.description()).to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
-        expect(addressString.description()).to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
+        expect(addressString.description())
+            .to.equal('7v91N7iZ9mNicL8WfG6cgSCKyRXydQjLh6UYBWwm6y1Q');
     });
-
 });
